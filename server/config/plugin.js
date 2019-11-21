badbd8f0e86aabb5e5932848d9e30c6ceb142bd7
wyh <yale_wan@163.com>
2019-11-21 19:35:58 +0800

    vue-shop

---
+++ server/config/plugin.js
@@ -0,0 +1,13 @@
+'use strict';
+exports.mongoose = {
+    enable: true,
+    package: 'egg-mongoose',
+}
+exports.cors = {
+    enable: false,
+    package: 'egg-cors',
+};
+exports.ejs = {
+    enable: true,
+    package: 'egg-view-ejs',
+};
